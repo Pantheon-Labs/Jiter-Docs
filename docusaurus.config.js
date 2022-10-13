0a073e09d96017d29dffe1812d49f365ef01a1a6
Jose Valerio <contact@josevalerio.com>
2022-10-13 17:52:16 -0400

    Fixing  links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/Pantheon-Labs/Jiter-Docs",
+          editUrl: "https://github.com/Pantheon-Labs/Jiter-Docs/blob/main",
         },
         // blog: {
         //   showReadingTime: true,
