d2dadac16f4c39442646e735073a26db2d841ad8
Jose Valerio <contact@josevalerio.com>
2022-11-04 17:58:19 -0400

    Removed /docs from route

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/Pantheon-Labs/Jiter-Docs/blob/main",
+          routeBasePath: "/",
         },
         // blog: {
         //   showReadingTime: true,
